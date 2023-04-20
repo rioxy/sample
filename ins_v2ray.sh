7579434b642ec220665415c88e1c05b38f7758ef
rioxy <88192614+rioxy@users.noreply.github.com>
2023-04-20 11:31:28 +0700

    Update ins_v2ray.sh

--- ins_v2ray.sh
+++ ins_v2ray.sh
@@ -1,85 +1,68 @@
 #!/bin/bash
 
-# Update the system
-sudo apt update
+# Update package list
+apt update
 
-# Install necessary dependencies
-sudo apt install curl -y
+# Install required packages
+apt install -y curl unzip
 
-# Download and install v2ray
-curl -O https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip v2ray-linux-64.zip
-sudo mv v2ray /usr/local/bin/
-sudo mv v2ctl /usr/local/bin/
+# Download and install V2Ray
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 
-# Create a v2ray configuration file
-sudo mkdir /etc/v2ray
-sudo touch /etc/v2ray/config.json
-sudo chmod 777 /etc/v2ray/config.json
-
-# Generate a random UUID for v2ray
-uuid=$(cat /proc/sys/kernel/random/uuid)
-
-# Write v2ray configuration to config.json
-cat << EOF > /etc/v2ray/config.json
+# Create a new V2Ray configuration file
+cat > /usr/local/etc/v2ray/config.json <<EOF
 {
-    "inbounds": [{
-        "port": 10001, # Replace with your desired port
-        "protocol": "vmess",
-        "settings": {
-            "clients": [{
-                "id": "$uuid",
-                "alterId": 64
-            }]
-        },
-        "streamSettings": {
-            "network": "tcp",
-            "tcpSettings": {
-                "header": {
-                    "type": "http",
-                    "request": {
-                        "path": ["/"],
-                        "headers": {
-                            "Host": ["example.com"] # Replace with your desired host
-                        }
-                    }
-                }
+  "inbounds": [
+    {
+      "port": 8080,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "YOUR_UUID",
+            "alterId": 64
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "tcpSettings": {
+          "header": {
+            "type": "http",
+            "response": {
+              "version": "1.1",
+              "status": "200",
+              "reason": "OK",
+              "headers": {
+                "Content-Type": ["application/octet-stream", "application/x-msdownload", "text/mcf-xml", "text/xml", "application/atom+xml", "application/rss+xml", "application/x-javascript", "text/javascript", "text/css", "text/html", "application/x-silverlight-2", "application/x-silverlight-3", "application/x-silverlight-4", "application/futuresplash", "image/gif", "image/png", "image/jpeg", "image/bmp", "image/webp", "audio/mpeg", "audio/ogg", "audio/x-wav", "video/mp4", "video/ogg", "application/octet-stream", "application/pdf", "text/plain", "application/zip", "application/gzip"],
+                "Cache-Control": ["public, max-age=31536000, immutable"],
+                "Access-Control-Allow-Origin": ["*"],
+                "Vary": ["Accept-Encoding"],
+                "Server": ["V2Fly"],
+                "Content-Encoding": ["gzip"],
+                "Transfer-Encoding": ["chunked"],
+                "Connection": ["keep-alive"]
+              }
             }
+          }
         }
-    }, {
-        "port": 10002, # Replace with another desired port
-        "protocol": "vmess",
-        "settings": {
-            "clients": [{
-                "id": "$uuid",
-                "alterId": 64
-            }]
-        },
-        "streamSettings": {
-            "network": "tcp",
-            "tcpSettings": {
-                "header": {
-                    "type": "http",
-                    "request": {
-                        "path": ["/"],
-                        "headers": {
-                            "Host": ["example.com"] # Replace with your desired host
-                        }
-                    }
-                }
-            }
-        }
-    }],
-    "outbounds": [{
-        "protocol": "freedom",
-        "settings": {}
-    }]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
 }
 EOF
 
-# Start v2ray service
-sudo systemctl enable v2ray
-sudo systemctl start v2ray
+# Replace YOUR_UUID with a new UUID
+sed -i 's/YOUR_UUID/'$(cat /proc/sys/kernel/random/uuid)'/g' /usr/local/etc/v2ray/config.json
 
-# Print v2ray status
-sudo systemctl status v2ray
+# Start V2Ray service
+systemctl start v2ray
+systemctl enable v2ray
+
+# Print V2Ray status
+systemctl status v2ray
